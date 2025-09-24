1dd1a9c5b2a5591ae823407911a1ad11941095d3
ArthurJuwer <arthur.juwer99@gmail.com>
2025-09-24 11:39:20 -0300

    Corregido uma seção onde nescessitava responsividade

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -4,23 +4,24 @@ import SocialMediaCard from "./SocialMediaCard"
 
 const SocialMedia = () => {
   return (
-    <div id="redes"  className="relative bg-mainBackground h-[78vh] lg:h-[84vh] 2xl:h-[58vh]">
-        <svg className="absolute lg:-top-1 z-0 w-full h-auto" viewBox="0 0 1918 465" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M942.746 464L1 140.614V1H1917V56.1964L942.746 464Z" fill="#EBFFC9" stroke="#EBFFC9"/>
-        </svg>
-        <div className="absolute -translate-x-1/2 left-1/2 top-0 z-50 w-11/12 lg:w-9/12 flex flex-col justify-center lg:shadow-neoBrutalism_right shadow-neoBrutalism_right_mobile rounded-xl">
+    <div id="redes"  className="relative flex items-center flex-col bg-mainBackground 2xl:pb-24 xl:pb-20 pb-14">
+        <div
+        className="absolute top-0 w-full aspect-[1918/465] z-0 bg-[#EBFFC9]"
+        style={{ 
+            clipPath: "polygon(49.2% 100%, 0% 30.3%, 0% 0%, 100% 0%, 100% 12.1%, 49.2% 100%)" 
+        }}
+        ></div>
+        <div className="z-50 w-11/12 xl:w-10/12 2xl:w-9/12 flex flex-col justify-center lg:shadow-neoBrutalism_right shadow-neoBrutalism_right_mobile rounded-xl">
             <div className="p-6 z-40 bg-thirdGreen text-center rounded-xl border-[3px] border-black">
                 <h1 className="font-bold text-2xl">Me siga nas Redes Sociais </h1>
                 
             </div>
-            <div className="-mt-6 w-full flex justify-center gap-4 flex-wrap lg:justify-between py-12 px-3 lg:py-12 lg:px-28 bg-secondGreen text-center rounded-xl border-2 border-r-0 border-t-0 border-black">
+            <div className="-mt-6 w-full flex justify-center gap-4 flex-col lg:flex-row lg:justify-between py-12 px-3 lg:py-12 lg:px-20 2xl:px-28 bg-secondGreen text-center rounded-xl border-2 border-r-0 border-t-0 border-black">
                 
-                <SocialMediaCard icon={<Github className="lg:size-20 size-10" />} link="github.com/ArthurJuwer" followers="15 seguidores"/>
-                <SocialMediaCard icon={<Linkedin className="lg:size-20 size-10" />} link="linkedin.com/in/arthurjuwer/" followers="224 conexões"/>
-                <SocialMediaCard icon={<Instagram className="lg:size-20 size-10" />} link="instagram.com/arthur_juwer" followers="821 seguidores"/>
+                <SocialMediaCard icon={<Github className="lg:size-20 size-10" />} link="github.com/ArthurJuwer" followers="30 seguidores"/>
+                <SocialMediaCard icon={<Linkedin className="lg:size-20 size-10" />} link="linkedin.com/in/arthurjuwer/" followers="854 conexões"/>
+                <SocialMediaCard icon={<Instagram className="lg:size-20 size-10" />} link="instagram.com/arthur_juwer" followers="865 seguidores"/>
 
-                
-                
             </div>
         </div>
     </div>
--- src/components/SocialMedia/SocialMediaCard.tsx
+++ src/components/SocialMedia/SocialMediaCard.tsx
@@ -8,11 +8,11 @@ interface PropsSocialMediaCard{
 
 const SocialMediaCard: FunctionComponent<PropsSocialMediaCard> = ({icon, link, followers}) => {
   return (
-    <div className="w-[calc(50%-0.8rem)] lg:w-auto flex flex-col items-center gap-y-4">
-        <div className="lg:size-56 size-28 bg-boxGray rounded-md text-secondGreen flex items-center justify-center">{icon}</div>
-        <a href={`https://www.${link}`} target="_blank" className="underline text-boxGray lg:text-xl text-xs">{link}</a>
+    <div className="lg:w-auto flex flex-col items-center gap-y-4">
+        <div className="size-56 bg-boxGray rounded-md text-secondGreen flex items-center justify-center">{icon}</div>
+        <a href={`https://www.${link}`} target="_blank" className="underline text-boxGray text-xl">{link}</a>
         <div className="bg-boxGray px-4 py-1 rounded-2xl lg:w-4/5">
-            <h1 className="text-secondGreen font-medium text-xs lg:text-base">{followers}</h1>
+            <h1 className="text-secondGreen font-medium text-base">{followers}</h1>
         </div>
     </div>
   )
